d72f4ed3d8f29343af98e69356046b79c16721d1
Vivek Kumar <32337899+vickytilotia@users.noreply.github.com>
2021-01-26 21:08:17 +0530

    contact form

--- simpleweb/home/models.py
+++ simpleweb/home/models.py
@@ -8,3 +8,10 @@ class Contact(models.Model):
     email = models.CharField(max_length=122)
     phone = models.IntegerField()
     desc =models.TextField()
+
+
+    def __str__(self):
+        # it will display the contact name in django admin contact list
+        # instead of object1, object1
+        return self.name
+    
